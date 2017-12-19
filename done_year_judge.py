04c5186fbcc9cb6979cf08c908334e34ee01b346
vbtbear <vbtbear@github.com>
2017-12-19 17:59:17 +0800

    yes
    yes

--- done_year_judge.py
+++ done_year_judge.py
@@ -21,7 +21,8 @@ else:
 '''
 #函数判断
 
-def year_judge(y):
+y =int(input("请输入： "))
+def year_judge1(y):
     if (y % 4) == 0:
         if (y % 100) == 0:
             if (y % 400) == 0:
@@ -32,8 +33,7 @@ def year_judge(y):
             return True # 非整百年能被4整除的为闰年
     else:
         return False
-print(year_judge(1900))
-
+print('year_judge1',year_judge1(y))
 
 def year_judge2(y):
     if y % 400 == 0:
@@ -42,6 +42,39 @@ def year_judge2(y):
         return True
     else:
         return False
-y =int(input("请输入"))
-print ( year_judge2 ( y ) )
+print ( 'year_judge2',year_judge2 ( y ) )
+
+def year_judge3(y):
+    if ((y % 4 ==0 and y % 100 !=0) or y % 400 == 0):
+        return True
+    else:
+        return False
+print('year_judge3',year_judge3(y))
+
+def year_judge4(y):#这个是个错误例子
+    if y % 400 == 0 and y % 4 ==0 or y % 100 !=0:
+        return True
+    else:
+        return False
+print('year_judge4',year_judge4(y),'这是个错误例子')
+
+def year_judge5(y):
+    if y % 100 == 0:
+        return y % 400 ==0
+    else:
+        return y % 4 == 0
+print('year_judge5',year_judge5(y))
+
+def year_judge6(y):
+    if y % 100 == 0:
+        if y % 400 ==0:
+            return True
+        else:
+            return False
+    else:
+        if y % 4 == 0:
+            return True
+        else:
+            return False
+print('year_judge6',year_judge6(y))
 
